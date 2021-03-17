3b9044585ad8f994f640d12969ab3297e52c8297
Shaher-11 <shahershamroukh@gmail.com>
2021-03-17 15:07:55 +0200

    Fix linters

--- main.rb
+++ main.rb
@@ -1,13 +1,17 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
   i = arr.length - 1
 
-  i.times do 
-    arr.each do | idx |
-      if arr[idx] > arr[idx+1]
-        arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
+  i.times do
+    (0...i).each do |idx|
+      if arr[idx] > arr[idx + 1]
+        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
         p arr
       end
     end
+    i -= 1
+  end
 end
 
 arr = [2, 1, 3, 5, 4, 7, 6]
