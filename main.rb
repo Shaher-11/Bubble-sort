b33bf1b8f7ea858c685ff609ed040c71dc4ee373
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-03-17 15:54:32 +0300

    Use times to deal with comparison of nil integer error

--- main.rb
+++ main.rb
@@ -1,12 +1,14 @@
 def bubble_sort(arr)
-  # iterate over array with item and index
-  arr.each_with_index do |n, idx|
-    if arr[idx] > arr[idx+1]
-      arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
-    end
-  end
-end
+  i = arr.length - 1
 
+  i.times do 
+    arr.each do | idx |
+      if arr[idx] > arr[idx+1]
+        arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
+        p arr
+      end
+    end
+end
 
 arr = [2, 1, 3, 5, 4, 7, 6]
 bubble_sort(arr)
